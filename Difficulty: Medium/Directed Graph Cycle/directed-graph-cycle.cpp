16d8f1125d8afce8ef5c0e6601bcb6cc81d209b7
Santhosh S <115558976+Santhosh-12345@users.noreply.github.com>
2024-09-14 01:35:54 -0700

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -4,41 +4,42 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    bool dfs(int node,vector<int>& vis,vector<int>& pathvis,vector<int> adj[])
-    {
-        vis[node]=1;
-        pathvis[node]=1;
-        for(auto it:adj[node])
-        {
-            if(!vis[it])
-            {
-                if(dfs(it,vis,pathvis,adj)==true)
-                return true;
-            }
-            else if(pathvis[it]==1)
-            {
-                return true;
-            }
-        }
-        pathvis[node]=0;
-        return false;
-    }
   public:
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        vector<int>vis(V,0);
-        vector<int>pathvis(V,0);
+        vector<int>indegree(V,0);
         for(int i=0;i<V;i++)
         {
-            if(!vis[i])
+            for(auto it:adj[i])
             {
-                if(dfs(i,vis,pathvis,adj)==true)
-                return true;
+                indegree[it]++;
             }
         }
+        queue<int>q;
+        vector<int>topo;
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        while(!q.empty())
+        {
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+        if(topo.size()==V)
         return false;
+        return true;
     }
 };
 
